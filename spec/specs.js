8042594243fb7b446b53b71749b612a48d8affa6
BoboTheAvatar <bobotheavatar@gmail.com>
2018-11-13 08:06:44 +0200

    Jasmine Test Setup

--- spec/specs.js
+++ spec/specs.js
@@ -2,7 +2,7 @@
 const Jasmine= require('jasmine');
 const JasmineConsoleReporter =require('jasmine-console-reporter');
 
-const jasmine = new Jasmine();
+const jasmine = new Jasmine(); 
 const reporter = new JasmineConsoleReporter({
         colors: 1,
         cleanStack: 3,
